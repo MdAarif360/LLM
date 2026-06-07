4f45bf9de9341313c90a247cf3c37a747b28945f
MdAarif360 <144222493+MdAarif360@users.noreply.github.com>
2026-06-07 16:49:27 +0300

    Update llm_provider.py

--- llm_provider.py
+++ llm_provider.py
@@ -1,11 +1,21 @@
 import os
+import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv
 
-load_dotenv()
+def get_openai_api_key():
+    # 1. Streamlit Cloud secrets
+    if "OPENAI_API_KEY" in st.secrets:
+        return st.secrets["OPENAI_API_KEY"]
+
+    # 2. Local environment variable fallback
+    return os.getenv("OPENAI_API_KEY")
+    
+
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+#load_dotenv()
 
 def ask_llm(prompt):
     response = client.responses.create(
